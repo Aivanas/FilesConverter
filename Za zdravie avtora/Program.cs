4ed965e6dbe3243185be03557beae7a0683d723c
Aivanas <vk53418006@gmail.com>
2022-12-05 21:54:37 +0300

    Initial commit

---
+++ Za zdravie avtora/Program.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Za_zdravie_avtora;
+using System.Xml.Serialization;
+
+Main_function();
+//C:\Users\richb\Desktop\gchg.txt
+
+static void Main_function()
+{
+    Console.WriteLine("Введите путь изначального файла: ");
+    string path_start_file = Console.ReadLine();
+    //string path_start_file = "C:\\Users\\richb\\Desktop\\gchg.txt";
+    string format = (path_start_file[path_start_file.Length - 1]).ToString();
+
+
+    while (true)
+    {
+        if (format == "t")
+        {
+            List<string> readed_txt = new List<string>(File.ReadAllLines(path_start_file));
+            if (readed_txt.Count() >= 3)
+            {
+                List<Humans> humansList = new List<Humans>();
+                for (int i = 0; i < (readed_txt.Count() / 3); i++)
+                {
+                    Humans human = new Humans();
+                    int was = i * 3;
+
+                    human.Name = readed_txt[was];
+                    human.Age = readed_txt[was + 1];
+                    human.ID = readed_txt[was + 2];
+                    humansList.Add(human);
+                }
+
+                Chooser_and_editor chooser = new Chooser_and_editor();
+                chooser.chooser(readed_txt, humansList);
+
+                break;
+            }
+
+
+            else
+            {
+                Console.WriteLine("Выбранный файл пуст :(");
+            }
+        }
+
+        if (format == "n")
+        {
+            string text = File.ReadAllText(path_start_file);
+            List<Humans> humansList = JsonConvert.DeserializeObject<List<Humans>>(text);
+            List<string> studentsList = new List<string>();
+            foreach (Humans human in humansList)
+            {
+                studentsList.Add(human.Name);
+                studentsList.Add(human.Age);
+                studentsList.Add(human.ID);
+            }
+
+            Chooser_and_editor chooser = new Chooser_and_editor();
+            chooser.chooser(studentsList, humansList);
+            break;
+        }
+
+        if (format == "l")
+        {
+            List<string> studentsList = new List<string>();
+            List<Humans> humansList = new List<Humans>();
+            XmlSerializer xml = new XmlSerializer(typeof(Humans));
+            using (FileStream fs = new FileStream(path_start_file, FileMode.Open))
+            {
+                Humans human = (Humans)xml.Deserialize(fs);
+                humansList.Add(human);
+                studentsList.Add(human.Name);
+                studentsList.Add(human.Age);
+                studentsList.Add(human.ID);
+            }
+
+
+            Chooser_and_editor chooser = new Chooser_and_editor();
+            chooser.chooser(studentsList, humansList);
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Чет не то вы ввели. Не обнаружен ни один из поддерживаемых форматов.");
+        }
+    }
+}
---
+++ Za zdravie avtora/Students.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Serialization;
+
+namespace Za_zdravie_avtora
+{
+    public class Humans
+    {
+        public string Name;
+        public string Age;
+        public string ID;
+    }
+
+    public class Chooser_and_editor
+    {
+        public void chooser(List<string> students_list, List<Humans> humansList)
+        {
+            int line_num = 0;
+
+            while (true)
+            {
+                Console.Clear();
+
+                for (int i = 0; i < students_list.Count(); i++)
+                {
+                    if (i == line_num)
+                    {
+                        Console.WriteLine(students_list[i] + " <-- ");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(students_list[i]);
+                    }
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.UpArrow & line_num > 0)
+                {
+                    line_num--;
+                }
+                else if (keyInfo.Key == ConsoleKey.DownArrow & line_num < students_list.Count - 1)
+                {
+                    line_num++;
+                }
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    Console.Write(students_list[line_num] + " --> ");
+                    students_list[line_num] = Convert.ToString(Console.ReadLine());
+                }
+                else if (keyInfo.Key == ConsoleKey.F1)
+                {
+                    saver(students_list, humansList);
+                    break;
+                }
+            }
+        }
+
+        private static void saver(List<string> students_list, List<Humans> humansList)
+        {
+            Console.Clear();
+            Console.WriteLine("Введите путь по которому сохранить файл: ");
+            string save_path = Console.ReadLine();
+            string format = (save_path[save_path.Length - 1]).ToString();
+            if (format == "t")
+            {
+                File.WriteAllLines(save_path, students_list);
+            }
+            else if (format == "n")
+            {
+                string serilized = JsonConvert.SerializeObject(humansList);
+                File.WriteAllText(save_path, serilized);
+            }
+            else if (format == "l")
+            {
+                XmlSerializer serialized = new XmlSerializer(typeof(Humans));
+                using (FileStream fs = new FileStream(save_path, FileMode.OpenOrCreate))
+                {
+                    foreach (var human in humansList)
+                    {
+                        serialized.Serialize(fs, human);
+                    }
+                }
+            }
+        }
+    }
+}
